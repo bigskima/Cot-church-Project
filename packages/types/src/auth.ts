f8b6a148c3ae19c3ee28860fad72bdd49d285bec
bigskima <our2026project@gmail.com>
2026-08-24 18:12:36 +0100

    feat(types): add authentication contracts

---
+++ packages/types/src/auth.ts
@@ -0,0 +1,11 @@
+export interface SignupRequest {
+  email: string;
+  phoneNumber: string;
+  password: string;
+}
+
+export interface UserProfile {
+  id: string;
+  email: string;
+  phoneNumber: string;
+}
