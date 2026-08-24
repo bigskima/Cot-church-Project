91459189a32d2d50fdbe533d97493b0f58587d0d
bigskima <our2026project@gmail.com>
2026-08-24 18:12:32 +0100

    feat(mobile): add signup screen foundation

---
+++ apps/mobile/app/(auth)/signup.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function SignupScreen() {
+  return (
+    <View>
+      <Text>Sign up with email and mobile number</Text>
+    </View>
+  );
+}
