1e63045fb00ad4ed9678cb3cdfafbaa4bffd031e
bigskima <our2026project@gmail.com>
2026-09-07 21:51:46 +0100

    Redirect legacy Live to General COT

--- apps/mobile/app/live/index.tsx
+++ apps/mobile/app/live/index.tsx
@@ -1,1 +1,6 @@
-export { default } from '../(tabs)/live/index';
+import React from 'react';
+import { Redirect } from 'expo-router';
+
+export default function LegacyLiveRoute() {
+  return <Redirect href="/general/live" />;
+}
