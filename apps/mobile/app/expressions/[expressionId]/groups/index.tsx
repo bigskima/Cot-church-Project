5761b4870184d0a29bda546fb0459abfd53031a0
bigskima <our2026project@gmail.com>
2026-09-07 18:50:05 +0100

    Add dedicated Expression groups route

---
+++ apps/mobile/app/expressions/[expressionId]/groups/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ExpressionGroupsExperience } from '@/features/expression/ExpressionGroupsExperience';
+
+export default function ExpressionGroupsScreen() {
+  return <ExpressionGroupsExperience embedded />;
+}
