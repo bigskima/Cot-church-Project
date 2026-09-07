87b87f7dc9c6624638bf055390c824ba67f33333
bigskima <our2026project@gmail.com>
2026-09-07 19:43:40 +0100

    Add canonical General Community route

---
+++ apps/mobile/app/general/community.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CommunityExperience } from '@/features/community/CommunityExperience';
+
+export default function GeneralCommunityScreen() {
+  return <CommunityExperience scope="general" />;
+}
