1ae500329db0543be9bf9c73fab557ef00ae3ac0
bigskima <our2026project@gmail.com>
2026-09-07 02:12:28 +0100

    Export full comments thread

--- apps/mobile/src/components/index.ts
+++ apps/mobile/src/components/index.ts
@@ -28,4 +28,5 @@ export * from './media/ReelPlayer';
 export * from './cards/VideoCard';
 export * from './cards/ReelCard';
 export * from './engagement/CommentSheet';
+export * from './engagement/CommentsThread';
 export * from './engagement/ReactionDrawer';
