5f93f594314410a1f8f768fc42dd566c6737b5cd
bigskima <our2026project@gmail.com>
2026-09-07 19:50:48 +0100

    Add canonical General post detail

---
+++ apps/mobile/app/general/post/[id].tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import CommunityPostScreen from '../../../post/[id]';
+
+export default function GeneralPostDetailScreen() {
+  return <CommunityPostScreen forcedScope="general" />;
+}
