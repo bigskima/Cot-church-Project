da393b6f0bb905951a12ce1d590bb256003c57c9
bigskima <our2026project@gmail.com>
2026-09-07 18:07:22 +0100

    Add dedicated Expression birthdays route

---
+++ apps/mobile/app/expressions/[expressionId]/birthdays.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ExpressionBirthdaysExperience } from '@/features/expression/ExpressionBirthdaysExperience';
+
+export default function ExpressionBirthdaysScreen() {
+  return <ExpressionBirthdaysExperience embedded />;
+}
