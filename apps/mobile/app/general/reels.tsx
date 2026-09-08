720801723bb93bc34fc127cd7082db73aaa74d0b
bigskima <our2026project@gmail.com>
2026-09-08 06:27:59 +0100

    Remove duplicate General primary route

--- apps/mobile/app/general/reels.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ReelsExperience } from '@/features/media/ReelsExperience';
-
-export default function GeneralReelsScreen() {
-  return <ReelsExperience scope="general" />;
-}
