7199c15a0aa767c40fb18d4d6c29b15844d4e291
bigskima <our2026project@gmail.com>
2026-09-08 06:27:49 +0100

    Move General primary screen into main tabs

---
+++ apps/mobile/app/general/(main)/community.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { CommunityExperience } from '@/features/community/CommunityExperience';
+export default function GeneralCommunityScreen() { return <CommunityExperience scope="general" />; }
