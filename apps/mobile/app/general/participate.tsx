53a72e49a8958f5e029f5683bbf8640efe57e4c8
bigskima <our2026project@gmail.com>
2026-09-14 02:43:05 +0100

    Expose General COT polls and giveaways

---
+++ apps/mobile/app/general/participate.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CommunityParticipationExperience } from '@/features/community/CommunityParticipationExperience';
+
+export default function GeneralParticipationScreen() {
+  return <CommunityParticipationExperience scope="general" />;
+}
