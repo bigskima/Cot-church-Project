368a0eab0aef33583999a8533e153655bf8f432f
bigskima <our2026project@gmail.com>
2026-09-16 05:03:58 +0100

    Add General COT location route

---
+++ apps/mobile/app/general/location.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { PublicLocationExperience } from '@/features/location/PublicLocationExperience';
+
+export default function GeneralLocationScreen() {
+  return <PublicLocationExperience scope="general" />;
+}
