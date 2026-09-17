0580c1ede642bd72590f74bb078fcf81406fd505
bigskima <our2026project@gmail.com>
2026-09-17 23:41:12 +0100

    centralize explicit keyboard behavior for ios android and web

---
+++ apps/mobile/src/utils/keyboard.ts
@@ -0,0 +1,22 @@
+import { Platform, type KeyboardAvoidingViewProps, type ScrollViewProps } from 'react-native';
+
+export const PLATFORM_KEYBOARD_BEHAVIOR: KeyboardAvoidingViewProps['behavior'] = Platform.select({
+  ios: 'padding',
+  android: 'height',
+  web: 'padding',
+  default: 'padding',
+});
+
+export const PLATFORM_KEYBOARD_DISMISS_MODE: ScrollViewProps['keyboardDismissMode'] = Platform.select({
+  ios: 'interactive',
+  android: 'on-drag',
+  web: 'none',
+  default: 'on-drag',
+});
+
+export const PLATFORM_KEYBOARD_VERTICAL_OFFSET = Platform.select({
+  ios: 0,
+  android: 0,
+  web: 0,
+  default: 0,
+});
