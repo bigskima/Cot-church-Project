86327d751a872e227fdc9e62cc2f5d44aa1df095
bigskima <our2026project@gmail.com>
2026-09-20 14:51:58 +0100

    feat: add Library and Devotional routes

---
+++ apps/mobile/app/general/leadership/devotionals-manage.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { DevotionalManageExperience } from '@/features/library/DevotionalManageExperience';
+export default function DevotionalManageRoute(){return <DevotionalManageExperience/>;}
