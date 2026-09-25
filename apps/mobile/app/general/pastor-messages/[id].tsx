d6e9cb16f9589e39b7b82a0a98949b279bdb95c7
bigskima <our2026project@gmail.com>
2026-09-25 12:31:15 +0100

    route public General Pastor Messages to dedicated detail screen

--- apps/mobile/app/general/pastor-messages/[id].tsx
+++ apps/mobile/app/general/pastor-messages/[id].tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
-import { EnhancedSermonDetailExperience } from '@/features/media/EnhancedSermonDetailExperience';
+import PastorMessageDetailExperience from '@/features/media/PastorMessageDetailExperience';
 
 export default function GeneralPastorMessageDetailScreen() {
   const { id } = useLocalSearchParams<{ id?: string }>();
-  return <EnhancedSermonDetailExperience sermonId={typeof id === 'string' ? id : ''} scope="general" pastorMessage />;
+  return <PastorMessageDetailExperience sermonId={typeof id === 'string' ? id : ''} scope="general" />;
 }
